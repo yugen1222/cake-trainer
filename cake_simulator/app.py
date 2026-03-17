5af88a9c0d5c40063995b6f565c3125ba1a42945
yugen1222 <134773110+yugen1222@users.noreply.github.com>
2026-03-17 13:07:37 +0500

    app.py

--- cake_simulator/app.py
+++ cake_simulator/app.py
@@ -8,10 +8,7 @@ from cake_simulator.database import init_db, save_result, get_results_by_employe
 app = Flask(__name__)
 app.secret_key = "CHANGE_ME_TO_SOMETHING_RANDOM_123"  # ?? ????? ?????? ???
 
-
-@app.before_first_request
-def setup():
-    init_db()
+init_db()
 
 
 @app.get("/")
@@ -132,5 +129,4 @@ def results():
 
 
 if __name__ == "__main__":
-    init_db()
     app.run(debug=True)
